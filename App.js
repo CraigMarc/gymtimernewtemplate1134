ff2022da57b2dcd06ec00ecdfe006755f2e318fa
craigmarc <cmarcinkiewicz2000@yahoo.com>
2025-07-28 22:01:09 -0400

    changed template and updated to android version 35

---
+++ App.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react'
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Countdown from './Countdown.js'
+
+const App = () => {
+
+  //states
+  const [timer, setTimer] = useState("03:00");
+  const [running, setRunning] = useState(true)
+  const [button, setButton] = useState("Start")
+  const [buttonColor, setButtonColor] = useState("green")
+
+
+  return (
+    <>
+      <Countdown
+        timer={timer}
+        setTimer={setTimer}
+        running={running}
+        setRunning={setRunning}
+        button={button}
+        setButton={setButton}
+        buttonColor={buttonColor}
+        setButtonColor={setButtonColor}
+      />
+
+    </>
+  )
+
+}
+
+export default App;
